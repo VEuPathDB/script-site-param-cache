fb21c631baef8b4f119213b80a364261081ca484
Elizabeth Harper <foxcapade@gmail.com>
2020-05-04 18:46:18 -0400

    updated wdk lib dependency, improved verbose logging

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	wdk "github.com/VEuPathDB/lib-go-wdk-api/v0"
 	"gopkg.in/yaml.v3"
 	"os"
 	"time"
@@ -30,8 +31,12 @@ func main() {
 	case 0: log.SetLevel(log.ErrorLevel)
 	case 1: log.SetLevel(log.WarnLevel)
 	case 2: log.SetLevel(log.InfoLevel)
-	case 3: log.SetLevel(log.DebugLevel)
-	case 4: log.SetLevel(log.TraceLevel)
+	case 3:
+		log.SetLevel(log.DebugLevel)
+		wdk.Logger().SetLevel(log.DebugLevel)
+	case 4:
+		log.SetLevel(log.TraceLevel)
+		wdk.Logger().SetLevel(log.TraceLevel)
 	}
 	log.Info("Running param exerciser")
 
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/Foxcapades/Argonaut v0.1.7
 	github.com/Foxcapades/Go-ChainRequest v2.7.4+incompatible
 	github.com/VEuPathDB/lib-go-rest-types v0.1.6
-	github.com/VEuPathDB/lib-go-wdk-api v0.0.20
+	github.com/VEuPathDB/lib-go-wdk-api v0.0.21-0.20200504224318-305d4120ee43
 	github.com/gammazero/workerpool v0.0.0-20200311205957-7b00833861c6
 	github.com/mattn/go-colorable v0.1.6 // indirect
 	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
